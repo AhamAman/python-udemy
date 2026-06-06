0ac29a3637d5d33578ca66124ff45629bd61c38f
AhamAman <dasamankumar50@gmail.com>
2026-06-06 11:17:31 +0530

    modified data types

--- concept/02_datatypes/chapter_2.py
+++ concept/02_datatypes/chapter_2.py
@@ -1,8 +1,21 @@
+'''
+Mutable vs Immutable Data Types
+'''
+
+# 1. Immutable Example (String)
+chef_name = "Chef Ranveer"
+print(f"Initial Name: {chef_name} | ID: {id(chef_name)}")
+
+# We "add" to the string
+chef_name = chef_name + " Brar"
+print(f"Updated Name: {chef_name} | ID: {id(chef_name)}")
+
+print("-" * 40)
+
+# 2. Mutable Example (Your Set)
 spice_mix = set()
-print(f"Initial spice mix id: {id(spice_mix)}")
-print(f"Initial spice mix id: {spice_mix}")
+print(f"Initial Mix : {spice_mix}       | ID: {id(spice_mix)}")
+
+# We add to the set
 spice_mix.add("Ginger")
-spice_mix.add("cardamom")
-spice_mix.add("lemon")
-print(f"Initial spice mix id: {spice_mix}")
-print(f"After spice mix id: {id(spice_mix)}")
+print(f"Updated Mix : {spice_mix} | ID: {id(spice_mix)}")
--- concept/02_datatypes/chapter_5.py
+++ concept/02_datatypes/chapter_5.py
@@ -1,3 +1,5 @@
+''' Chapter 5: Floating Point Numbers'''
+
 import sys
 from fractions import Fraction
 from decimal import Decimal
@@ -8,4 +10,6 @@ current_temp = 95.49
 print(f"Ideal temp { ideal_temp }")
 print(f"Current temp { current_temp }")
 print(f"Difference temp { ideal_temp - current_temp }")
+
+#see how interpretor treats the floating point numbers
 print(sys.float_info)
